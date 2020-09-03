b5a16bef192f6bd46dd333a5ff9e8c79ac9bb428
Jazmin Matos <jmatos1993@gmail.com>
2020-09-03 01:28:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,38 +1,13 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
   def initialize(title)
     @title = title
   end
-  
-  def title #no setter method necessary b/c we're 'setting' it when we initialize and won't be resetting it
-    @title
-  end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
  
-  def genre
-    @genre
-  end
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+ 
 end
-
